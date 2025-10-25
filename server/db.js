d61f7ff9fc6721bb0eb86df2bce989260a8f3f8e
pyramidsmartsales-hue <pyramids.mart.sales@gmail.com>
2025-10-25 06:05:20 +0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,13 +1,12 @@
-import pkg from 'pg';
-const { Pool } = pkg;
+// db.js
 import dotenv from 'dotenv';
+import pkg from 'pg';
 dotenv.config();
-
+const { Pool } = pkg;
 
 const pool = new Pool({
-connectionString: process.env.DATABASE_URL,
-ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
-
 
 export default pool;
